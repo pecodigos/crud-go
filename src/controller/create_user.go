d0b8ecb56e80b66d6bfee7c10b2461f4b350f6b6
pecodigos <pedrenriquecunha@gmail.com>
2024-11-20 04:29:14 -0300

    Implementing createUser

--- src/controller/create_user.go
+++ src/controller/create_user.go
@@ -1,22 +1,33 @@
 package controller
 
 import (
+	"crud-go/src/configuration/validation"
 	"crud-go/src/controller/model/request"
-	"crud-go/src/rest_err"
+	"crud-go/src/controller/model/response"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
 )
 
 func CreateUser(c *gin.Context) {
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
-		restErr := rest_err.NewBadRequestError(
-			fmt.Sprintf("Incorrect field, error=%s", err.Error))
+		log.Printf("Error trying to marshal object, error=%s\n", err.Error())
+		restErr := validation.ValidateUserError(err)
 
 		c.JSON(restErr.Code, restErr)
 		return
 	}
 
 	fmt.Println(userRequest)
+	userResponse := response.UserResponse{
+		ID:    "test",
+		Email: userRequest.Email,
+		Name:  userRequest.Name,
+		Age:   userRequest.Age,
+	}
+
+	c.JSON(http.StatusOK, userResponse)
 }
