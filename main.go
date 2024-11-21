e9310f5c30b70222b7b07b20755f9aba6b2a710b
pecodigos <pedrenriquecunha@gmail.com>
2024-11-21 00:44:24 -0300

    creating logs

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"crud-go/src/configuration/logger"
 	"crud-go/src/controller/routes"
 	"github.com/gin-gonic/gin"
 	"log"
 )
 
 func main() {
+	logger.Info("About to start app.")
 	router := gin.Default()
 
 	routes.InitRoutes(&router.RouterGroup)
--- src/controller/create_user.go
+++ src/controller/create_user.go
@@ -1,33 +1,39 @@
 package controller
 
 import (
+	"crud-go/src/configuration/logger"
 	"crud-go/src/configuration/validation"
 	"crud-go/src/controller/model/request"
 	"crud-go/src/controller/model/response"
-	"fmt"
 	"github.com/gin-gonic/gin"
-	"log"
+	"go.uber.org/zap"
 	"net/http"
 )
 
 func CreateUser(c *gin.Context) {
+	logger.Info("Init CreateUser controller.",
+		zap.String("journey", "createUser"),
+	)
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
-		log.Printf("Error trying to marshal object, error=%s\n", err.Error())
+		logger.Error("Error trying to validate user info.", err)
 		restErr := validation.ValidateUserError(err)
 
 		c.JSON(restErr.Code, restErr)
 		return
 	}
 
-	fmt.Println(userRequest)
 	userResponse := response.UserResponse{
 		ID:    "test",
 		Email: userRequest.Email,
 		Name:  userRequest.Name,
 		Age:   userRequest.Age,
 	}
+
+	logger.Info("User created successfully",
+		zap.String("journey", "createUser"),
+	)
 
 	c.JSON(http.StatusOK, userResponse)
 }
